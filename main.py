c2751784515ccb1bd1a5baf86a5a6c6fb2b03fb9
ahmadjonovabdulaziz700-star <ahmadjonovabdulaziz700@gmail.com>
2025-12-02 10:39:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -70,3 +70,15 @@ def handle_message(message):
 
 
 bot.infinity_polling()
+import os
+from flask import Flask
+
+app = Flask(name)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+if name == "main":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
