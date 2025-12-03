d313a929fd4760bb0e99fa6e0cc8570e8d251731
ahmadjonovabdulaziz700-star <ahmadjonovabdulaziz700@gmail.com>
2025-12-03 09:15:53 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# ==============================
+#     BOT UCHUN SOZLAMALAR
+# ==============================
+
+# Bot token — bu joyga tokenni o'zing qo'yasan
+BOT_TOKEN = "8581347222:AAGqbvDRflviOVKhwdwGkejLEy5LaDgld-o"
+
+# Admin ID — bu joyga o'zingni Telegram ID qo'yasan
+ADMIN_ID = 8581347222:AAGqbvDRflviOVKhwdwGkejLEy5LaDgld-o
